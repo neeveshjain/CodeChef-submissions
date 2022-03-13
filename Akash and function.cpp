8bd7826f3aa4cb4451956e2f332bab49147afa09
Neevesh Jain <85023234+neeveshjain@users.noreply.github.com>
2022-03-13 20:52:14 +0530

    Create Akash and function.cpp

---
+++ Akash and function.cpp
@@ -0,0 +1,17 @@
+// Akash and Function 
+// Akash has his maths test tomorrow. He secretly found out one of the questions from the test. As the test is online, he wants to write a code that solves the question. However, he is not good at coding and thus, wants Chef to help him with that.
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int tc;
+    cin>>tc;
+    while (tc--)
+    {
+        int num;
+        cin>>num;
+        num++;
+        cout<<(num>>1)<<endl;
+    }
+    return 0;
+}
