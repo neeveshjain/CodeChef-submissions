fff5482285c3bc6b6c32ceac0b0f7c2a701c92f3
Neevesh Jain <85023234+neeveshjain@users.noreply.github.com>
2022-03-14 16:33:03 +0530

    Create tyre problem.cpp

---
+++ tyre problem.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int tc;
+    cin>>tc;
+
+    while (tc--)
+    {
+        int N,M;
+        cin>>N>>M;
+        cout<<N*2+M*4<<endl;
+
+    }
+    return 0;
+
+}
