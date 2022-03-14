d290af341a88b22c637671c5b655a77c7893b14f
Neevesh Jain <85023234+neeveshjain@users.noreply.github.com>
2022-03-14 16:32:14 +0530

    Create Chef and instant noodles.cpp

---
+++ Chef and instant noodles.cpp
@@ -0,0 +1,14 @@
+// Chef and Instant Noodles
+// Chef has invented 1-minute Instant Noodles. As the name suggests, each packet takes exactly 1 minute to cook.
+
+// Chef's restaurant has X stoves and only 1 packet can be cooked in a single pan.
+
+// How many customers can Chef serve in Y minutes if each customer orders exactly 1 packet of noodles?
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cin>>x>>y;
+    cout<<x*y<<endl;
+    return 0;
+}
